985f3827dcaf9acd4c4b86db8e42c75bf0349cfe
Ariyadi Armi <ariyadiarmi06@gmail.com>
2023-01-26 11:43:27 +0700

    update2

--- routes/EmployeeRoute.js
+++ routes/EmployeeRoute.js
@@ -8,10 +8,13 @@ const router = express.Router();
 
 router.get("/getInfoEmployee", getEmployee);
 router.get("/api/v1/employee", auth, getEmployee);
-router.get("/api/v1/employee/:id", auth, getEmployeeByID);
+// router.get("/api/v1/employee/:id", auth, getEmployeeByID);
+router.get("/api/v1/employee/:id", getEmployeeByID);
 // router.post("/api/v1/employee", validationDaftar, auth, saveEmployee);
 router.post("/api/v1/employee", validationDaftar, saveEmployee);
-router.patch("/api/v1/employee/:id", validationDaftar, auth, updateEmployee);
-router.delete("/api/v1//employee/:id", auth, deleteEmployee);
+// router.patch("/api/v1/employee/:id", validationDaftar, auth, updateEmployee);
+router.patch("/api/v1/employee/:id", validationDaftar, updateEmployee);
+// router.delete("/api/v1//employee/:id", auth, deleteEmployee);
+router.delete("/api/v1//employee/:id", deleteEmployee);
 
 export default router;
